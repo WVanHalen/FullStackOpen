f7a96861e191b44a35395031a2012997641de71f
Werner Vanhala <vanhala64@gmail.com>
2024-05-24 21:41:43 +0300

    unicafe valmis

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -1,12 +1,77 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  if (props.all == 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  return (
+    <table>
+      <thead>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine
+          text="average"
+          value={(props.good - props.bad) / props.all}
+        />
+        <StatisticLine
+          text="positive"
+          value={(props.good / props.all) * 100 + "%"}
+        />
+      </thead>
+    </table>
+  );
+};
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
-  return <div>code here</div>;
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+    </div>
+  );
 };
 
 export default App;
