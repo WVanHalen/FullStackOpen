d357d8ef9c4706333646a8508cfb8732a9361468
Werner Vanhala <vanhala64@gmail.com>
2024-08-21 10:11:43 +0300

    Tehtävät 4.1 ja 4.2

--- part 4/blogilista/app.js
+++ part 4/blogilista/app.js
@@ -1,43 +1,59 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
+/*const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+}); */
 
-const Blog = mongoose.model('Blog', blogSchema)
+//const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+//const mongoUrl = "mongodb://localhost/bloglist";
+mongoose.set("strictQuery", false);
 
-app.use(cors())
-app.use(express.json())
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error.message);
+  });
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use("/api/blogs", blogsRouter);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-const PORT = 3003
+/*app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});*/
+
+module.exports = app;
