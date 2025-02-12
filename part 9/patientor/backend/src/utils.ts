7b54371f91bf23e42f421d7fd35aebb384b77fb0
Werner Vanhala <vanhala64@gmail.com>
2025-02-12 11:12:59 +0200

    9.14 done

--- part 9/patientor/backend/src/utils.ts
+++ part 9/patientor/backend/src/utils.ts
@@ -1,50 +1,10 @@
-import { Gender, NewPatient } from "./types";
+import { Gender } from "./types";
+import { z } from "zod";
 
-const isString = (text: unknown): text is string => {
-  return typeof text === "string" || text instanceof String;
-};
-
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender)
-    .map((v) => v.toString())
-    .includes(param);
-};
-
-const parseData = (data: unknown): string => {
-  if (!isString(data)) {
-    throw new Error("Incorrect or missing data");
-  }
-  return data;
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!isString(gender) || !isGender(gender)) {
-    throw new Error("Incorrect or missing gender");
-  }
-  return gender;
-};
-
-const toNewPatient = (object: unknown): NewPatient => {
-  if (!object || typeof object !== "object") {
-    throw new Error("Incorrect or missing data");
-  }
-  if (
-    "name" in object &&
-    "dateOfBirth" in object &&
-    "ssn" in object &&
-    "gender" in object &&
-    "occupation" in object
-  ) {
-    const newPatient: NewPatient = {
-      name: parseData(object.name),
-      dateOfBirth: parseData(object.dateOfBirth),
-      ssn: parseData(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseData(object.occupation),
-    };
-    return newPatient;
-  }
-  throw new Error("Incorrect data: some fields are missing");
-};
-
-export default toNewPatient;
+export const NewPatientSchema = z.object({
+  name: z.string(),
+  dateOfBirth: z.string(),
+  ssn: z.string(),
+  gender: z.nativeEnum(Gender),
+  occupation: z.string(),
+});
